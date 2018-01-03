e1d81243595b77e1eeb2bae8191ff332fe3a421c
Ata Hakcil <ahakcil@gmail.com>
2018-01-04 02:53:37 +0300

    Optimized

--- Asci Game/Engine.h
+++ Asci Game/Engine.h
@@ -1,12 +1,20 @@
+//Thanks to the javidx9 (AKA oneLoneCoder) for inspiring me to create this
+//
+//
+//
+//
+//
+//
 #pragma once
 #include <ctime>
 #include <vector>
+#include <list>
 #include <Windows.h>
 #include <iostream>
 #include <thread>
 #include "List.h"
 
-int fpslock = 180; //max fps (game speed)
+double fpslock = 50; //max fps (game speed)
 int delayMultip = 1;
 
 #define  LPSTR          char*						//just to be safe about CreateConsoleBuffer
@@ -44,15 +52,15 @@ public:
 	AsciiEngine(int nScreenWidth, int nScreenHeight);
 	void AppendToActors(Actor* newActor);
 	void DrawFrame();
-	void DrawAttribute();
 	void ComposeFrame();
 	void StartGame();
 	void tick();
 	void tickTEntry(Actor* Actor);
 	void FrameTEntry(struct s_node* cur);
+	void ComposeTitle(double framelength);
 private:
 	
-
+	CHAR_INFO *screenChar;
 	wchar_t *screen;
 	WORD* screenatb;
 	HANDLE hConsole;
@@ -68,11 +76,8 @@ private:
  /////////////////////
 AsciiEngine::AsciiEngine(int nScreenWidth, int nScreenHeight)
 {
-	//this creates screen Buffer
-	screen = new wchar_t[nScreenWidth*nScreenHeight];
-	screenatb = new WORD[nScreenWidth*nScreenHeight];
-	for (int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
-	for (int j = 0; j < nScreenWidth*nScreenHeight; j++) screenatb[j] = BACKGROUND;
+	//this creates screen Buffer 
+	screenChar = new CHAR_INFO[nScreenWidth*nScreenHeight];
 	hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	dwBytesWritten = 0;
@@ -82,44 +87,55 @@ AsciiEngine::AsciiEngine(int nScreenWidth, int nScreenHeight)
 	g_AllActors.head = g_AllActors.tail = NULL; //initialize actors list
 
 }
-char title[50];
+
 /////////////////////
 void AsciiEngine::StartGame() {
+
+	double framelength = 0;
+	double frameDelay = (double)1000.0 / (double)fpslock;
+	int totalframes = 0;
+	
+	while (g_HP>0)			//basically game loop
+	{
+		clock_t begin = clock();
+		std::thread title(&AsciiEngine::ComposeTitle, this, framelength);
+		std::thread delay(Sleep,frameDelay);
+		totalframes++;
+		tick();					//plays every actors ActorTick
+		ComposeFrame();			//creates screen buffer
+		std::thread draw(&AsciiEngine::DrawFrame, this);//draws the frame, but starts after tick and composeFrame has finished
+		//DrawFrame();
+
+		draw.join();	//every thread should synch for frame to end
+		delay.join();
+		title.join();
+		clock_t end = clock();
+		framelength = ((double)end - (double)begin) / (double)CLOCKS_PER_SEC;	//this calculates length of a frame from tick start to end of delay
+	}
+}
+
+char title[50];
+void AsciiEngine::ComposeTitle(double framelength)
+{
 
 	char name[50] = "Ascii Engine ";//
 	char fps[15] = "(FPS: ";		//
 	char fpsf[10];					//
 	char hp[5] = "HP: ";
-	double framelength=0;
-	double frameDelay = (double)1000.0 / (double)fpslock;
-	int totalframes = 0;
 
-	while (g_HP>0)			//basically game loop
-	{
-		totalframes++;
-		clock_t begin = clock();					//This part composes window title and frame counter
-		strcpy(title, "");							//
-		strcpy(fpsf, "");							//
-		sprintf(fpsf, "%.2lf", 1.0/framelength);	//
-		strcat(title,name);							//
-		strcat(title, fps);							//
-		strcat(title, fpsf);						//
-		strcat(title, ") ");						//
-		strcat(title, hp);
-		for (int i = 0; i < g_HP; i++)
-			strcat(title, "*");
-		SetConsoleTitle(title);
-
-		tick();					//plays every actors ActorTick
-		ComposeFrame();			//creates screen buffer
-		DrawFrame();			//prints the screen buffer to console
-		DrawAttribute();
-		Sleep(frameDelay);
-
-		clock_t end = clock();
-		framelength = ((double)end - (double)begin) / (double)CLOCKS_PER_SEC;	//this calculates length of a frame from tick start to end of delay
-	}
+	strcpy(title, "");							////This part composes window title and frame counter
+	strcpy(fpsf, "");							//
+	sprintf(fpsf, "%.2lf", 1.0 / framelength);	//
+	strcat(title, name);						//
+	strcat(title, fps);							//
+	strcat(title, fpsf);						//
+	strcat(title, ") ");						//
+	strcat(title, hp);
+	for (int i = 0; i < g_HP; i++)
+		strcat(title, "*");
+	SetConsoleTitle(title);
 }
+
 void AsciiEngine::tickTEntry(Actor* Actor)
 {
 	Actor->ActorTick();
@@ -127,39 +143,24 @@ void AsciiEngine::tickTEntry(Actor* Actor)
 /////////////////////
 void AsciiEngine::tick()
 {
-	//std::vector<std::thread> threads;
-	struct s_node* cur = g_AllActors.head;	
+	struct s_node* cur = g_AllActors.head;
 	while (cur != NULL)	//traverse the list containing every actor
 	{
-		//threads.push_back(std::thread(&AsciiEngine::tickTEntry,this,cur->thisActor));	
 		cur->thisActor->ActorTick();
 		cur = cur->next;
 	}
-	//for (auto &t : threads)
-	//	t.join();
 }
-
-
 
 /////////////////////
 void AsciiEngine::DrawFrame()
 {
-	WriteConsoleOutputCharacterW(					//prints screen buffer
-		hConsole,						//handle
-		screen,							//characters
-		m_nScreenWidth*m_nScreenHeight, //size
-		{ 0,0 },						//struct _COORD from WINAPI
-		&dwBytesWritten);				//reference to number of bytes written		
-}
-
-void AsciiEngine::DrawAttribute()
-{
-	WriteConsoleOutputAttribute(
-		hConsole,						//handle
-		screenatb,						//Attribute
-		m_nScreenWidth*m_nScreenHeight,	//length
-		{ 0,0 },						//struct _COORD from WINAPI
-		&dwAttrWritten);				//reference to number of attributes written
+	SMALL_RECT writeZone = { 0, 0, (short)m_nScreenWidth, (short)m_nScreenHeight };
+	WriteConsoleOutputW(					//prints screen buffer
+		hConsole,							//handle
+		screenChar,							//characters
+		{ (short)m_nScreenWidth, (short)m_nScreenHeight }, //size
+		{ 0,0 },							//struct _COORD from WINAPI
+		&writeZone);					//reference to number of bytes written		
 }
 
 /////////////////////
@@ -168,19 +169,18 @@ void AsciiEngine::ComposeFrame()
 	struct s_node* cur = g_AllActors.head;
 	std::vector<std::thread> threads;
 	int pos;
-	for (int i = 0; i < m_nScreenWidth*m_nScreenHeight-1; i++) screen[i] =L' '; //fill the buffer with empty space
-	for (int j = 0; j < m_nScreenWidth*m_nScreenHeight - 1; j++) screenatb[j] = BACKGROUND; //fill the buffer with empty space
+	for (int i = 0; i < m_nScreenWidth*m_nScreenHeight - 1; i++) {
+		screenChar[i].Char.UnicodeChar = L' '; //fill the buffer with empty space
+		screenChar[i].Attributes = BACKGROUND; //fill the buffer with empty space
+	}
+
 	while (cur != NULL)					//traverse the list containing every actor
 	{
 		int pos = cur->thisActor->GetPosition().onScreenPos;
-		screen[pos] = cur->thisActor->GetTag(); //set position of actor to its tag
-		screenatb[pos] = cur->thisActor->GetAttribute();	//and its attribute
-		//threads.push_back(std::thread(&AsciiEngine::FrameTEntry, this, cur));
+		screenChar[pos].Char.UnicodeChar = cur->thisActor->GetTag(); //set position of actor to its tag
+		screenChar[pos].Attributes = cur->thisActor->GetAttribute();	//and its attribute
 		cur = cur->next;
 	}
-	
-	//for (auto &t : threads)
-	//	t.join();
 }
 
 void AsciiEngine::FrameTEntry(struct s_node* cur)
@@ -200,27 +200,6 @@ void AsciiEngine::AppendToActors(Actor* newActor) {	//get freshly created actor 
 	node->next = g_AllActors.head;
 	g_AllActors.head = node;
 }
-
-
-
-
-/*
-void AsciiEngine::AppendToActors(Actor* newActor) {	//get freshly created actor appended to the linked list
-struct s_node* node;
-node = (struct s_node*)malloc(sizeof(struct s_node));
-node->thisActor = newActor;
-if (g_AllActors.head == NULL) {
-g_AllActors.head = node;
-g_AllActors.tail = node;
-return;
-}
-g_AllActors.tail->next = node;
-g_AllActors.tail = node;
-
-}
-*/
-
-
 
 #include "Player.h"
 #include "ActorVariants.h"
